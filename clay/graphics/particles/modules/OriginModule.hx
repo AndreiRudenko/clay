11cb2fd198a6d1b2b33221754c611224542e8e8d
rudenkoarts <rudenkopaint@gmail.com>
2019-09-24 03:18:22 +0300

    Add particles OriginModule

---
+++ clay/graphics/particles/modules/OriginModule.hx
@@ -0,0 +1,97 @@
+package clay.graphics.particles.modules;
+
+import clay.graphics.particles.core.ParticleModule;
+import clay.graphics.particles.core.Particle;
+import clay.graphics.particles.core.Components;
+import clay.graphics.particles.components.Origin;
+import clay.math.Vector;
+
+using clay.graphics.particles.utils.VectorExtender;
+
+
+class OriginModule extends ParticleModule {
+
+
+	public var initialOrigin(default, null):Vector;
+	public var initialOriginMax:Vector;
+
+	var _origin:Components<Origin>;
+
+
+	public function new(_options:OriginModuleOptions) {
+
+		super(_options);
+
+		initialOrigin = _options.initialOrigin != null ? _options.initialOrigin : new Vector(0.5, 0.5);
+		initialOriginMax = _options.initialOriginMax;
+		
+	}
+
+	override function init() {
+
+		_origin = emitter.components.get(Origin);
+
+	}
+
+	override function onSpawn(p:Particle) {
+		
+		var o:Vector = _origin.get(p.id);
+
+		if(initialOriginMax != null) {
+			o.x = emitter.randomFloat(initialOrigin.x, initialOriginMax.x);
+			o.y = emitter.randomFloat(initialOrigin.y, initialOriginMax.y);
+		} else {
+			o.x = initialOrigin.x;
+			o.y = initialOrigin.y;
+		}
+		
+	}
+
+
+// import/export
+
+	override function fromJson(d:Dynamic) {
+
+		super.fromJson(d);
+
+		initialOrigin.fromJson(d.initialOrigin);
+
+		if(d.initialOriginMax != null) {
+			if(initialOriginMax == null) {
+				initialOriginMax = new Vector();
+			}
+			initialOriginMax.fromJson(d.initialOriginMax);
+		}
+		
+
+		return this;
+	    
+	}
+
+	override function toJson():Dynamic {
+
+		var d = super.toJson();
+
+		d.initialOrigin = initialOrigin.toJson();
+
+		if(initialOriginMax != null) {
+			d.initialOriginMax = initialOriginMax.toJson();
+		}
+
+		return d;
+	    
+	}
+
+
+}
+
+
+typedef OriginModuleOptions = {
+	
+	>ParticleModuleOptions,
+	@:optional var initialOrigin:Vector;
+	@:optional var initialOriginMax:Vector;
+
+}
+
+
