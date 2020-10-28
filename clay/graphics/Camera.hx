3f1fa232240031a53d1b1486834b8eb062162258
rudenkoarts <rudenkopaint@gmail.com>
2020-10-28 13:51:48 +0200

    Add Camera

---
+++ clay/graphics/Camera.hx
@@ -0,0 +1,69 @@
+package clay.graphics;
+
+import clay.math.Vector2;
+import clay.math.FastMatrix3;
+import clay.utils.Math;
+import clay.graphics.Texture;
+
+class Camera {
+
+	public var x:Float = 0;
+	public var y:Float = 0;
+
+	public var width:Float = 0;
+	public var height:Float = 0;
+
+	public var rotation:Float = 0;
+
+	public var originX:Float = 0;
+	public var originY:Float = 0;
+
+	public var offsetX:Float = 0;
+	public var offsetY:Float = 0;
+
+	public var zoom:Float = 1;
+
+	public var projection:FastMatrix3 = new FastMatrix3();
+	public var view:FastMatrix3 = new FastMatrix3();
+	public var invProjectionView:FastMatrix3 = new FastMatrix3();
+
+	public function new(x:Float = 0, y:Float = 0, width:Float = 0, height:Float = 0) {
+		this.x = x;
+		this.y = y;
+		this.width = width > 0 ? width : Clay.window.width;
+		this.height = height > 0 ? height : Clay.window.height;
+	}
+
+	public function update() {		
+		if (Texture.renderTargetsInvertedY) {
+			projection.orto(0, width, 0, height);
+		} else {
+			projection.orto(0, width, height, 0);
+		}
+
+		view.setTransform(x+offsetX, y+offsetY, Math.radians(rotation), zoom, zoom, originX, originY, 0, 0);
+		invProjectionView.copyFrom(view);
+		invProjectionView.invert();
+
+		projection.append(invProjectionView);
+	}
+
+	public function screenToWorld(v:Vector2, ?into:Vector2):Vector2 {
+		if(into == null) into = new Vector2();
+		
+		into.copyFrom(v);
+		into.set(view.getTransformX(v.x, v.y), view.getTransformY(v.x, v.y));
+
+		return into;
+	}
+
+	public function worldToScreen(v:Vector2, ?into:Vector2):Vector2 {
+		if(into == null) into = new Vector2();
+		
+		into.copyFrom(v);
+		into.set(invProjectionView.getTransformX(v.x, v.y), invProjectionView.getTransformY(v.x, v.y));
+
+		return into;
+	}
+
+}
