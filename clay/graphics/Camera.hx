5340320e858d0cbbc1e2f14eaefdf618a8f0545a
rudenkoarts <rudenkopaint@gmail.com>
2020-11-09 13:40:03 +0200

    Add Camera check inside methods to for culling

--- clay/graphics/Camera.hx
+++ clay/graphics/Camera.hx
@@ -64,5 +64,53 @@ class Camera {
 		into.set(invProjectionView.getTransformX(v.x, v.y), invProjectionView.getTransformY(v.x, v.y));
 		return into;
 	}
+
+	public function pointInside(px:Float, py:Float) {
+		// convert world to screen coords
+		var tmp:Float = px;
+		px = view.getTransformX(px, py);
+		py = view.getTransformY(tmp, py);
+
+		if(px < 0 || px > width) return false;
+		if(py < 0 || py > height) return false;
+
+		return true;
+	}
+	
+	public function rectangleInside(rx:Float, ry:Float, rw:Float, rh:Float) {
+		// convert world to screen coords
+		var minX = view.getTransformX(rx, ry);
+		var minY = view.getTransformY(rx, ry);
+
+		var maxX = view.getTransformX(rx+rw, ry+rh);
+		var maxY = view.getTransformY(rx+rw, ry+rh);
+
+		if (x + width < minX || x > maxX) return false;
+		if (y + height < minY || y > maxY) return false;
+
+		return true;
+	}
+
+	public function circleInside(cx:Float, cy:Float, cr:Float) {
+		// convert world to screen coords
+		cr = view.getTransformY(0, cr); // TODO: probably wrong
+		var tmp:Float = cx;
+		cx = view.getTransformX(cx, cy);
+		cy = view.getTransformY(tmp, cy);
+
+		var cpX:Float = cx;
+		var cpY:Float = cy;
+
+		if (cpX < x) cpX = x;
+		if (cpX > x + width) cpX = x + width;
+		if (cpY < y) cpY = y;
+		if (cpY > y + height) cpY = y + height;
+
+	    var diffX:Float = cx - cpX;
+	    var diffY:Float = cy - cpY;
+
+	    return (diffX * diffX + diffY * diffY < cr * cr);
+	}
+
 	
 }
