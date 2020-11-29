b653654f1c16bd17961d3b39d9a8f352219e937e
rudenkoarts <rudenkopaint@gmail.com>
2020-11-29 17:40:52 +0200

    Minor formating shader changes

--- assets/shaders/multitextured.vert.glsl
+++ assets/shaders/multitextured.vert.glsl
@@ -17,6 +17,6 @@ void main() {
 	gl_Position = vec4((projectionMatrix * vec3(position, 1.0)).xy, 0.0, 1.0);
 	outColor = color;
 	outTexCoord = texCoord;
-    outTexId = texId;
-    outTexFormat = texFormat;
+	outTexId = texId;
+	outTexFormat = texFormat;
 }
